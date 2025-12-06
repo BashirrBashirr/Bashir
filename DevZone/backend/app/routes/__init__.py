b27d63eef7d0865ead172d93489ffbf05f84de62
BashirrBashirr <takbir3332210@gmail.com>
2025-12-06 16:16:04 -0300

    Delete DevZone directory

--- DevZone/backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .users import router as users_router
-from .posts import router as posts_router
-from .comments import router as comments_router
-from .feed import router as feed_router
-
-__all__ = ["users_router", "posts_router", "comments_router", "feed_router"]
